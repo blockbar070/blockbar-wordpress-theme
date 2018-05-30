5186b1c78e075f5ff08c06caa5b4ebee7f644e8a
bartwr <mail@bartroorda.nl>
2018-05-30 16:15:57 +0200

    Only show posts from the "Blockbar Blog" category

--- posts.php
+++ posts.php
@@ -1,6 +1,8 @@
 <div class="posts-overview">
 
-  <?php while (have_posts('posts_per_page=100')) : the_post(); ?>
+  <?php
+  $query = new WP_Query('cat=53');
+  while ($query->have_posts('posts_per_page=100')) : $query->the_post(); ?>
   <div class="post">
 
     <a href="<?php the_permalink(); ?>" id="post-<?php the_ID(); ?>" class="post-title" data-id="<?php the_id(); ?>">
